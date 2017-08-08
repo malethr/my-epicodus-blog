e4600852761f4e4abfe91d380dc652fa968d41fd
Beth Hansen and Paul Guevarra <beth@>
2017-08-08 10:11:58 -0700

    add file set up

---
+++ src/main/java/models/Post.java
@@ -0,0 +1,28 @@
+package models;
+
+import java.util.ArrayList;
+
+/**
+ * Created by Guest on 8/8/17.
+ */
+public class Post {
+   private final String content;
+   private static ArrayList<Post> instances = new ArrayList<>();
+
+    public Post(String content) {
+        this.content = content;
+        instances.add(this);
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public static ArrayList<Post> getAll(){
+        return instances;
+    }
+
+    public static void clearAllPosts(){
+        instances.clear();
+    }
+}
